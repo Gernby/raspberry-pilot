09ae8e2bac12e61758898fbe6ff3e940ca2bf919
Steve J <easternpa@gmail.com>
2020-07-03 09:17:46 -0400

    show new value and rm temp files

--- changevalue.sh
+++ changevalue.sh
@@ -17,5 +17,8 @@ cat /home/ubuntu/kegman.json | jq ".`cat /home/ubuntu/changeparam/param` = `cat 
 # delete prior kegman backup, backup the current kegman, and overwrite the current kegman with the new value set
 rm /home/ubuntu/kegman.json.old
 cp /home/ubuntu/kegman.json /home/ubuntu/kegman.json.old
-# mv /home/ubuntu/kegman.json.tmp /home/ubuntu/kegman.json
+mv /home/ubuntu/kegman.json.tmp /home/ubuntu/kegman.json
+rm /home/ubuntu/changeparam/oldvalue /home/ubuntu/changeparam/newvalue /home/ubuntu/changeparam/delta /home/ubuntu/changeparam/plus-minus
 
+cat /home/ubuntu/changeparam/param && cat /home/ubuntu/kegman.json | jq .`cat /home/ubuntu/changeparam/param`
+rm /home/ubuntu/changeparam/param

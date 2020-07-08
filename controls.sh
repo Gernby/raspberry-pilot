1e750a6f9f7726bf5728f27260b1162e9b88234b
Steve J <easternpa@gmail.com>
2020-07-08 14:51:22 -0400

    specify python3.7

--- controls.sh
+++ controls.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PYTHONPATH="$PWD"
 pkill -f controlsd
-python ~/raspilot/selfdrive/controls/controlsd.py & 
+python3.7 ~/raspilot/selfdrive/controls/controlsd.py & 
 pkill -f boardd
 ~/raspilot/selfdrive/boardd/boardd &
 pkill -f dashboard
-python ~/raspilot/dashboard.py &
+python3.7 ~/raspilot/dashboard.py &
 sleep 10
-bash ~/raspilot/fix_niceness.sh
+sh ~/raspilot/fix_niceness.sh

fe50aae113f4dd2108d6b32bb30de50bff65c975
Steve J <easternpa@gmail.com>
2020-07-09 10:12:22 -0400

    moved most steps to ansible

--- start_install_tf.sh
+++ start_install_tf.sh
@@ -9,26 +9,50 @@
 # mv ~/raspberry-pilot/start_install_tf.sh ~
 # sh start_install_tf.sh
 
-# every so often, please upload performance data for analysis using this command
+# every so often, please upload data logs for analysis using this command
 # cd ~/raspilot && PYTHONPATH=~/raspilot python3.7 upload_files.py
 
 # --- Install script starts here ---
+
+# Install Ansible to support the future direction
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible -y
+
 # rename the folder
 cd ~
 mv ~/raspberry-pilot ~/raspilot
 cd ~/raspilot
 
-# add grafana signing key and repository
-sudo apt-key add grafana.gpg.key
-sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+# Add grafana signing key and repository
+# sudo apt-key add grafana.gpg.key
+# sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+
+echo "Install grafana gpg signing key..."
+ansible localhost -v -b -m apt_key -a "file=/home/ubuntu/raspberry-pilot/grafana.gpg.key"
+
+echo "Add grafana repo..."
+ansible localhost -v -b -m apt_repository -a "repo='deb https://packages.grafana.com/oss/deb stable main'"
+
+echo "Install grafana server.."
+ansible localhost -v -b -m apt -a "name=grafana update_cache=yes"
+# sudo apt install grafana -y
 
 # update software from repository
 sudo apt update
 
-sudo apt install -y  network-manager
-#ansible localhost -b -m lineinfile -a "path=/etc/default/crda regexp='^REGDOMAIN=' line='REGDOMAIN=US'"
-sudo sed -i -e 's/REGDOMAIN=/REGDOMAIN=US/' /etc/default/crda
-sudo systemctl start NetworkManager
+# Install Network Manager
+# sudo apt install network-manager -y && sudo service NetworkManager start
+
+echo "Installing Network Manager for better WiFi management.."
+# sudo apt install -y  network-manager
+# sudo sed -i -e 's/REGDOMAIN=/REGDOMAIN=US/' /etc/default/crda
+ansible localhost -b -m apt -a "name=network-manager"
+ansible localhost -b -m lineinfile -a "path=/etc/default/crda regexp='^REGDOMAIN=' line='REGDOMAIN=US'"
+
+echo "Starting Network Manager.."
+# sudo systemctl start NetworkManager
+ansible localhost -b -m service -a "name=NetworkManager state=started"
 
 # force rescan of the available wifi networks
 # sudo nmcli dev wifi rescan
@@ -37,11 +61,14 @@ sudo systemctl start NetworkManager
 sudo nmcli d wifi connect $1 password $2
 
 # disable unattended upgrades
-sudo apt remove -y unattended-upgrades
+ansible localhost -b -m apt -a "name=unattended-upgrades state=absent"
+# sudo apt remove -y unattended-upgrades
 
 # update Ubuntu and clean up
-sudo apt full-upgrade -y
-sudo apt autoremove -y
+#sudo apt full-upgrade -y
+#sudo apt autoremove -y
+ansible localhost -v -b -m apt -a "upgrade=full"
+ansible localhost -v -b -m apt -a "autoremove=yes"
 
 # install dependencies
 sudo apt install -y build-essential make automake python3.7-dev python3-pip jq
@@ -55,18 +82,26 @@ sudo apt install -y capnproto opencl-headers autotools-dev uuid-dev libsodium-de
 sudo apt install -y libusb-dev cmake libnewlib-arm-none-eabi libhdf5-serial-dev hdf5-tools smbclient
 sudo apt install -y influxdb influxdb-client apt-transport-https software-properties-common adduser libfontconfig1 dfu-util
 
-# install and start grafana; reverse the comments below to perform a standard install of grafana
-sudo apt install -y grafana
-#wget https://dl.grafana.com/oss/release/grafana_6.6.2_arm64.deb
-#sudo dpkg -i grafana_6.6.2_arm64.deb
+# Ansible version for the future
+#ansible localhost -b -m apt -a "name=build-essential,make,automake,python3.7-dev,python3-pip,jq"
+#ansible localhost -b -m apt -a "name=openjdk-8-jdk,automake,autoconf,zip,unzip,libtool,swig,libpng-dev,zlib1g-dev,pkg-config"
+#ansible localhost -b -m apt -a "name=libhdf5-dev,bzip2,clang,git,libarchive-dev"
+#ansible localhost -b -m apt -a "name=libffi-dev,libglib2.0-0,libssl-dev,libswscale-dev"
+#ansible localhost -b -m apt -a "name=libusb-1.0-0,libzmq5-dev,ocl-icd-libopencl1,ocl-icd-opencl-dev"
+#ansible localhost -b -m apt -a "name=opencl-headers,get,checkinstall"
+#ansible localhost -b -m apt -a "name=libatlas-base-dev,libopenblas-base,libopenblas-dev,gcc,gfortran"
+#ansible localhost -b -m apt -a "name=capnproto,autotools-dev,uuid-dev,libsodium-dev,valgrind"
+#ansible localhost -b -m apt -a "name=libusb-dev,cmake,libnewlib-arm-none-eabi,libhdf5-serial-dev,hdf5-tools,smbclient"
+#ansible localhost -b -m apt -a "name=influxdb,influxdb-client,apt-transport-https,software-properties-common,adduser,libfontconfig1,dfu-util"
 
+# install and start grafana and influxdb (installed but temporarily disabled to reduce resource usage)
+sudo apt install -y grafana
 sudo /bin/systemctl daemon-reload
 #sudo /bin/systemctl enable grafana-server
 #sudo service influxdb start
 #sudo service grafana-server start
 
-#python3 -m pip install pipenv --user
-
+# Install the TensorFlow 2.2 components and dependencies
 wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.2.0/tensorflow-2.2.0-cp37-none-linux_aarch64.whl 
 python3.7 -m pip install cython
 python3.7 -m pip install tensorflow-2.2.0-cp37-none-linux_aarch64.whl
@@ -77,14 +112,16 @@ python3.7 -m pip install setproctitle
 python3.7 -m pip install cffi
 python3.7 -m pip install cython
 
-# folders for storing various hdf5 files 
+# Create folders for storing various hdf5 files; supports switching models via remote ssh commands
 mkdir ~/buttons
 mkdir ~/buttons/model-1
 mkdir ~/buttons/model-2
 mkdir ~/buttons/model-3
 mkdir ~/buttons/model-4
 
+# Kick off the final stage of the build
 cd
 cp ~/raspilot/finish_install.sh ~/
 sh ~/finish_install.sh
-# Exit SSH and log in again, since pipenv won't work without it
+
+echo "Please reboot, run top -u ubuntu, and look for boardd, dashboard, and controlsd"

6ed407d733a212576c9d81546b4cdef1d9c45c1a
President Gernby, of Earth <gernby@planet-gern.gern>
2020-07-18 16:21:06 +0000

    enabled ublox

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -5,9 +5,12 @@ pkill -f transcoderd
 pkill -f controlsd
 pkill -f boardd
 pkill -f dashboard
+pkill -f ubloxd
 python3.7 selfdrive/controls/transcoderd.py &
 python3.7 dashboard.py &
 python3.7 selfdrive/controls/controlsd.py & 
 python3.7 selfdrive/pandad.py &
 #selfdrive/boardd/boardd &
+selfdrive/locationd/ubloxd &
+
 

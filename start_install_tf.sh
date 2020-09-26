cac24063950588923f1d57338c5592220a51661c
Steve J <easternpa@gmail.com>
2020-09-26 13:00:58 -0400

    Placeholder for tf2.3
    Loaded up the install commands but they're commented out until we test.

--- start_install_tf.sh
+++ start_install_tf.sh
@@ -120,6 +120,10 @@ sudo /bin/systemctl daemon-reload
 # ansible localhost -b -m service -a "name=influxdb enabled=yes"
 # ansible localhost -b -m service -a "name=influxdb state=started"
 
+# Placeholder for upcoming TensorFlow 2.3 update
+# wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.3.0/tensorflow-2.3.0-cp37-none-linux_aarch64.whl
+# python -m pip install tensorflow-2.3.0-cp37-none-linux_aarch64.whl
+
 # Install the TensorFlow 2.2 components and dependencies
 wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.2.0/tensorflow-2.2.0-cp37-none-linux_aarch64.whl
 python3.7 -m pip install -U pip

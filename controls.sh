12464db491c08ff2559510834344c3a69686d47f
President Gernby, of Earth <gernby@planet-gern.gern>
2020-11-19 01:44:57 +0000

    improved system performance

--- controls.sh
+++ controls.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+cd ~/raspilot
 export PYTHONPATH="$PWD"
 pkill -f controlsd
 pkill -f pandad
 pkill -f boardd
 pkill -f ubloxd
-python ~/raspilot/selfdrive/controls/controlsd.py & 
-python ~/raspilot/selfdrive/pandad.py &
-#~/raspilot/selfdrive/boardd/boardd &
-~/raspilot/selfdrive/locationd/ubloxd &
+taskset -a --cpu-list 0,1 python ~/raspilot/selfdrive/controls/controlsd.py & 
+#python ~/raspilot/selfdrive/pandad.py &
+taskset -a --cpu-list 0,1 ~/raspilot/selfdrive/boardd/boardd &
+taskset -a --cpu-list 2,3 ~/raspilot/selfdrive/locationd/ubloxd &
 pkill -f dashboard
-python ~/raspilot/dashboard.py &
+taskset -a --cpu-list 2,3 python ~/raspilot/dashboard.py &
 sleep 10
 bash ~/raspilot/fix_niceness.sh

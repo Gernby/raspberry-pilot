bc7d31535601219dd7d81aca9a515bd5b79322fb
raspberry-pilot <70973288+raspberry-pilot@users.noreply.github.com>
2020-12-02 12:24:45 -0600

    Merry thanksmas (#33)
    * added dual history inputs
    
    * updated model strategy
    
    * more of the same
    
    * update install and tune
    
    Co-authored-by: President Gernby, of Earth <gernby@planet-gern.gern>

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -12,4 +12,4 @@ taskset -a --cpu-list 0,1 python ~/raspilot/selfdrive/pandad.py &
 #~/raspilot/selfdrive/boardd/boardd &
 taskset -a --cpu-list 2,3 ~/raspilot/selfdrive/locationd/ubloxd &
 #taskset -a --cpu-list 0,1 python ~/raspilot/selfdrive/controls/controlsd.py wait & 
-taskset -a --cpu-list 2,3 python ~/raspilot/dashboard.py &
+#taskset -a --cpu-list 2,3 python ~/raspilot/dashboard.py &

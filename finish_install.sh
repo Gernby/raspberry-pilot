06428d5e6e94a8dffa56c7b22bd8eb75572b1dc9
Jason West <jamw24@gmail.com>
2021-06-02 07:30:08 -0600

    Removed extra sudo crontab entry that was causing a duplicate to be added to the root crontab

--- finish_install.sh
+++ finish_install.sh
@@ -19,7 +19,7 @@ sudo udevadm control --reload-rules && sudo udevadm trigger
 #(crontab -l; echo "@reboot bash raspilot/launch_openpilot.sh";) | crontab -
 ansible-playbook ~/raspilot/crontab.yml
 crontab -l
-(sudo crontab -l; echo "@reboot sleep 60; bash /home/ubuntu/raspilot/fix_niceness.sh";) | sudo crontab -
+#(sudo crontab -l; echo "@reboot sleep 60; bash /home/ubuntu/raspilot/fix_niceness.sh";) | sudo crontab -
 sudo crontab -l
 
 sudo cp ~/raspilot/phonelibs/btcmd.txt /boot/firmware
@@ -29,7 +29,7 @@ sudo cp ~/raspilot/phonelibs/influxdb.conf /etc/influxdb/influxdb.conf
 sudo bash phonelibs/install_capnp.sh
 
 #sudo chown -R 1000:1000 ~/raspilot/node-red
-#sudo docker run -it --network host -v /home/ubuntu/raspilot/node-red:/data --name nodered-raspilot nodered/node-red 
+#sudo docker run -it --network host -v /home/ubuntu/raspilot/node-red:/data --name nodered-raspilot nodered/node-red
 
 #python3 -m pipenv --python 3.7
 #python3 -m pipenv install

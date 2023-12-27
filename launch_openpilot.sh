9ea0e6bedf114b93dbd3502036a12eab4f7369c2
Gernby <gernby2002@yahoo.com>
2023-12-27 15:10:39 -0600

    Update launch_openpilot to run GernbyMode.py

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-cd ~/raspilot
-export PYTHONPATH="$PWD"
+#cd ~/raspilot
+#export PYTHONPATH="$PWD"
 pkill -f transcoderd
 pkill -f controlsd
 pkill -f pandad
 pkill -f boardd
 pkill -f ubloxd
 pkill -f dashboard
-sleep 15
-taskset -a --cpu-list 2,3 python3 ~/raspilot/selfdrive/controls/transcoderd.py &
-taskset -a --cpu-list 0,1 python3 ~/raspilot/selfdrive/pandad.py &
-#taskset -a --cpu-list 0,1 ~/raspilot/selfdrive/boardd/boardd &
-taskset -a --cpu-list 0,1 python3 ~/raspilot/selfdrive/controls/controlsd.py &
-taskset -a --cpu-list 2,3 ~/raspilot/selfdrive/locationd/ubloxd &
-#sleep 15
-taskset -a --cpu-list 2,3 python3 ~/raspilot/dashboard.py &
+pkill -f GernbyMode
+python3 ~/raspilot/panda/examples/GernbyMode.py

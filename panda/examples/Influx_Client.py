3e082912fefa59265c05ffeaa7578c0988274b16
Gernby <gernby2002@yahoo.com>
2024-01-21 18:55:27 -0600

    Performance improvement using threading
    Performance improvement using threading

--- panda/examples/Influx_Client.py
+++ panda/examples/Influx_Client.py
@@ -11,17 +11,13 @@ class Influx_Client():
         self.t = []
 
     def InsertData(self, receiveTime, canData):
-        influxLineString = ""
-        for cData in canData:
-            influxLineString += (canFormatString % (self.user_id,cData[0],cData[1],abs(cData[2])>>32,abs(cData[2]) & 0xFFFFFFFF, receiveTime))
-        if influxLineString != "":
-            try:
-                def SendRequest(dataString=influxLineString):
-                    headers = { 'Content-type': 'application/octet-stream', 'Accept': 'text/plain' }
-                    influx.request("write",'POST', {'db':'carDB'}, dataString.encode('utf-8'), 204, headers)
-            
-                if len(self.t) > 0: print(self.t.pop(0))
-                self.t.append(threading.Thread(target=SendRequest,).run())
-                self.t[0].start()
-            except:
-                pass
+        try:
+            def SendRequest(data=canData, influxLineString = ""):
+                for d in data:
+                    influxLineString += (canFormatString % (self.user_id,d[0],d[1],d[2]>>32,d[2] & 0xFFFFFFFF, receiveTime))
+                headers = { 'Content-type': 'application/octet-stream', 'Accept': 'text/plain' }
+                influx.request("write",'POST', {'db':'carDB'}, influxLineString.encode('utf-8'), 204, headers)
+            if len(self.t) > 0: print(self.t.pop(0))
+            self.t.append(threading.Thread(target=SendRequest,).run())
+        except:
+            pass
